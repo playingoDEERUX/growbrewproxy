8d49d5c0f12e1f284e52b1666bd83831de6ca07d
playingoDEERUX <62474734+playingoDEERUX@users.noreply.github.com>
2020-06-15 23:44:16 +0200

    v1.5.5

--- GrowbrewProxy/MainForm.Designer.cs
+++ GrowbrewProxy/MainForm.Designer.cs
@@ -1174,7 +1174,7 @@
             this.vLabel.Name = "vLabel";
             this.vLabel.Size = new System.Drawing.Size(41, 15);
             this.vLabel.TabIndex = 8;
-            this.vLabel.Text = "V1.5.4";
+            this.vLabel.Text = "V1.5.5";
             // 
             // playerLogicUpdate
             // 
